010bcfe7b2045d01b4745cfd0c0b517e1eed7f7b
Elif Yaren TAT <elifyarentat@gmail.com>
2021-12-31 16:21:20 +0300

    inital commit

---
+++ script.js
@@ -0,0 +1,93 @@
+// import axios from 'axios';
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+import spn from "./spn.js";
+// console.log(sha256("recep"));
+const name = prompt("What's your name?")
+const encryptType = document.querySelector('input[name="customRadioInline"]:checked').value
+
+appendMessage('Bağlanıldı.',2)
+// console.log(encryptType)
+socket.emit('new-user', name)
+
+function hash(string,type) {
+    // console.log(string);
+    if(type == "sha")
+    return sha256(string)
+    else if(type == "spn")
+    return spn.encrypt(string)
+  }
+
+
+socket.on('chat-message', data=> {
+    let datas = JSON.parse(data.data)
+    console.log("data");
+    console.log(datas);
+    if (document.querySelector('input[name="customRadioInline"]:checked').value == "spn" && datas.encrypt == "spn") {
+        datas.content = spn.decrypt(datas.content)
+    }
+    appendMessage(data.name +": "+ datas.content , 1)
+})
+
+socket.on('user-connected', name=> {
+    appendMessage(name+ ' Bağlandı',2)
+})
+
+socket.on('user-disconnected', name=> {
+    appendMessage(name+ ' Ayrıldı',2)
+})
+
+messageForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage('Sen: '+ message,3)
+    let enType = document.querySelector('input[name="customRadioInline"]:checked').value
+    let data = {
+        content: hash(message,enType),
+        encrypt: enType
+    }
+    socket.emit('send-chat-message', JSON.stringify(data))
+    messageInput.value = ''
+})
+
+function appendMessage(message,align){
+    const messageElement = document.createElement('div')
+    messageElement.classList.add("card")
+    messageElement.classList.add("my-3")
+    messageElement.classList.add("w-50")
+    const cardBody = document.createElement("div")
+    cardBody.classList.add("card-body")
+    cardBody.classList.add("p-2")
+    cardBody.innerText = message
+    messageElement.append(cardBody)
+    const cardContainer = document.createElement("div")
+    cardContainer.classList.add("d-flex")
+    cardContainer.classList.add("flex-row")
+    cardContainer.classList.add("w-100")
+    var bg = "white";
+    if(align == 1){
+        align = "justify-content-start"
+        bg = "bg-secondary"
+    }
+    else if(align == 2){
+        align = "justify-content-center"
+        bg = "bg-info"
+    }
+    else if(align == 3) {
+        align = "justify-content-end"
+        bg = "bg-warning"
+    }
+    else {
+        align = "justify-content-start"
+        bg = "bg-dark"
+    }
+    cardContainer.classList.add(align)
+    messageElement.classList.add(bg)
+    messageElement.classList.add("text-white")
+    cardContainer.append(messageElement)
+    messageContainer.append(cardContainer)
+    messageContainer.scrollTop = messageContainer.scrollHeight
+}
---
+++ server.js
@@ -0,0 +1,52 @@
+
+import spn from "./spn.js";
+import sha256 from "js-sha256";
+import { Server } from 'socket.io';
+import express from 'express';
+import { createServer } from 'http';
+
+const app = express(); 
+const server = createServer(app);
+const io = new Server (server,{
+    cors:{
+        origins :'*:*'
+    }
+});
+server.listen(3000);
+
+
+// const { createHash } = require('crypto');
+function hash(string,type) {
+    // console.log(string);
+    if(type == "sha")
+    return sha256(string)
+    // return createHash('sha256').update(string).digest('hex');
+    else if(type == "spn")
+    return spn.encrypt(string)
+  }
+
+//   console.log(hash("alo","sha"))
+//   console.log(spn.encrypt("emir"));
+//   console.log(spn.decrypt("00101110011011111110111001010001"))
+  
+  
+const users = {}
+let datas = ""
+
+io.on('connection', socket=> {
+    console.log("new user")
+    //socket.emit('chat-message', 'Hello World')
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', data =>{
+        // data=JSON.parse(data);
+        // socket.broadcast.emit('chat-message',{message: hash(data.content,data.encrypt), name: users[socket.id]})
+        socket.broadcast.emit('chat-message',{data :data, name: users[socket.id]})
+    })
+    socket.on('disconnect-user', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
+})
